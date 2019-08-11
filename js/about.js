a2a4da924a29d283e99458293945f359c2c02e43
sergeevsite <andreyinoks93@yandex.ru>
2019-08-11 22:44:23 +0700

    Страница о компании

--- js/about.js
+++ js/about.js
@@ -1,23 +1,26 @@
 $(document).ready(function(){
+// Slick Slider
 
-  // Настройки слайдера about
-    var worksSlider =  $(".owl-carousel");
-    worksSlider.owlCarousel({
-      items: 3,
-      dots: false,
-      nav: true,
-      margin: 30,
-      animateOut: 'fadeOut',
-    });
-  
-    // $('#worksControlsLeft').click(function() {
-    //   worksSlider.trigger('prev.owl.carousel');
-    // })
-  
-    // $('#worksControlsRight').click(function() {
-    //   worksSlider.trigger('next.owl.carousel');
-    // })
-  
+  $('.wood-slider').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    speed: 300,
+    infinite: false,
+    nextArrow: '.wood-control__right',
+    prevArrow: '.wood-control__left'
   });
+
+  // $('.epoxy-slider').slick({
+  //   centerMode: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   variableWidth: true,
+  //   speed: 300,
+  //   infinite: false,
+  // });
+
+});
   
   
