3233fd9d00604e0b784f53ff7a6c7de04eadd5a8
sergeevsite <andreyinoks93@yandex.ru>
2019-08-17 15:29:26 +0700

    доработка сайта

--- mail.php
+++ mail.php
@@ -18,12 +18,12 @@ $extra = $_POST['userExtra'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'decorepoxy@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'zapana21'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'pochtasteam@inbox.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'yanochka20100911'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('decorepoxy@mail.ru'); // от кого будет уходить письмо?
+$mail->setFrom('pochtasteam@inbox.ru'); // от кого будет уходить письмо?
 $mail->addAddress('pochtasteam@inbox.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
